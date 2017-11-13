269b3aa9e716fcb50da436f5cdc8c6786cbc390c
Sergio <slop@sdi.es>
2017-11-13 13:42:16 +0100

    :Module structure Created

---
+++ mymodule/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'My Module',
+    'category': 'None',
+    'summary': 'This is an empty module for Odoo lessons',
+    'website': 'http://www.sdi.es',
+    'version': '0.1',
+    'description': """
+Empty Module
+========================
+
+        """,
+    'depends': ['base'],
+    'data': [
+        ''
+    ],
+    'installable': True,
+}
